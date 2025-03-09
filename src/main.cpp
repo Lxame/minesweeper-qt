c3143b7f284cc286f45d104d249402f5473cdcf2
Matvey Starkov <m.a.starkov@stud.mospolytech.ru>
2025-03-09 20:33:38 +0300

    initial window designed

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,8 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     MainWindow w;
     w.setWindowTitle("MineSweeper");
-    w.resize(800, 600);
+    //w.resize(800, 600);
+    w.setFixedSize(400, 300);
     w.show();
     return a.exec();
 }
--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,14 +1,28 @@
 #include "mainwindow.h"
 
 #include <QVBoxLayout>
+#include <QLabel>
 
 MainWindow::MainWindow(QWidget *parent) : QWidget(parent)
 {
     m_pStartBtn = new QPushButton("Start!");
-    m_pStartBtn->setMinimumSize(100, 20);
+    m_pStartBtn->setFixedSize(100, 25);
+
+    QLabel *pChooseDifficultyLbl = new QLabel("Choose difficulty");
+    pChooseDifficultyLbl->setAlignment(Qt::AlignCenter);
+
+    m_pDifficultyCmb = new QComboBox();
+    m_pDifficultyCmb->addItem("Easy");
+    m_pDifficultyCmb->addItem("Medium");
+    m_pDifficultyCmb->addItem("Hard");
+    m_pDifficultyCmb->setFixedSize(100, 25);
 
     QVBoxLayout *pLayout = new QVBoxLayout;
+    pLayout->setSpacing(10);
+    pLayout->setAlignment(Qt::AlignCenter);
     pLayout->addWidget(m_pStartBtn);
+    pLayout->addWidget(pChooseDifficultyLbl);
+    pLayout->addWidget(m_pDifficultyCmb);
 
     setLayout(pLayout);
 }
